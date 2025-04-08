71a9b3bfbe0ba7ee6138fe55af33bbb52a4a9e51
Sven Fabricius <sven.fabricius@livediesel.de>
2025-04-08 15:13:56 +0200

    fix filename

--- src/admin.php
+++ src/admin.php
@@ -49,7 +49,7 @@ class Admin
 
     public function subscribers_page()
     {
-        require_once __DIR__ . 'subscribers-table.php';
+        require_once __DIR__ . '/subscribers-table.php';
         require_once __DIR__ . '/../pages/subscribers.php';
     }
 
