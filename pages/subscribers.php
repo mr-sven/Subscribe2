8c25afc0b51bc3b0c44a65df5885296e1b93814a
Sven Fabricius <sven.fabricius@livediesel.de>
2025-04-08 16:51:00 +0200

    subscribers

--- pages/subscribers.php
+++ pages/subscribers.php
@@ -2,12 +2,25 @@
 <?php $table = new SMini\SubscribersTable(); ?>
 <div class="wrap">
     <h2><?= esc_html(get_admin_page_title()) ?></h2>
-    <?php $table->views(); ?>
     <form method="post">
-    <?php
-        $table->prepare_items();
-        $table->search_box('search', 'search_id');
-        $table->display();
-    ?>
+        <div>
+            <h2><?= esc_html__('Add Subscribers', SMLD) ?></h2>
+            <p>
+                <?= esc_html__('Enter addresses, one per line or comma-separated', SMLD) ?><br>
+                <textarea rows="2" cols="80" name="addresses"></textarea>
+            </p>
+            <p class="submit" style="border-top: none;">
+                <input type="submit" class="button-primary" name="subscribe" value="<?= esc_attr(__('Subscribe', SMLD)) ?>" />
+            </p>
+        </div>
+        <div>
+            <h2><?= esc_html__('Current Subscribers', SMLD) ?></h2>
+            <?php $table->views(); ?>
+            <?php
+                $table->prepare_items();
+                $table->search_box('search', 'search_id');
+                $table->display();
+            ?>
+        </div>
     </form>
 </div>
