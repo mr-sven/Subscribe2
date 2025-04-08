5608c88ff622744cb29775ac4b1e7f63ff705d1c
Sven Fabricius <sven.fabricius@livediesel.de>
2025-04-08 15:18:58 +0200

    table daza

--- src/subscribers-table.php
+++ src/subscribers-table.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace SMini;
 
-class SubscribersTable extends \WP_List_Table {
+class SubscribersTable extends \WP_List_Table
+{
+    // define $table_data property
+    private $table_data;
+
+    // Get table data
+    private function get_table_data() {
+        global $wpdb;
+        return $wpdb->get_results("SELECT * FROM $wpdb->smini", ARRAY_A);
+    }
 
     // Define table columns
     public function get_columns()
@@ -21,11 +30,27 @@ class SubscribersTable extends \WP_List_Table {
     // Bind table with columns, data and all
     public function prepare_items()
     {
+        //data
+        $this->table_data = $this->get_table_data();
+
         $columns = $this->get_columns();
         $hidden = array();
         $sortable = array();
         $this->_column_headers = array($columns, $hidden, $sortable);
 
         $this->items = [];
+    }
+
+    public function column_default($item, $column_name)
+    {
+        switch ($column_name) {
+            case 'id':
+            case 'email':
+            case 'active':
+            case 'ts':
+            case 'active_ts':
+            default:
+                return $item[$column_name];
+        }
     }
 }
