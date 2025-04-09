b71340ece1705ab8ce290fe9ac044908378db440
Sven Fabricius <sven.fabricius@livediesel.de>
2025-04-09 09:16:49 +0200

    widget options

--- src/widget.php
+++ src/widget.php
@@ -29,4 +29,33 @@ class Widget extends \WP_Widget
             $control_ops
         );
     }
+
+    public function form($instance)
+    {
+        $options = get_option('widget_subscribeMiniWidget');
+        if (false === $options) {
+            $defaults = [
+                'title'             => __('Subscribe Mini Widget', SMLD),
+            ];
+        } else {
+            $defaults = [
+                'title'             => $options['title'],
+            ];
+            delete_option('widget_subscribeMiniWidget');
+        }
+        // Code to obtain old settings too.
+        $instance = wp_parse_args((array) $instance, $defaults);
+
+        $title             = htmlspecialchars($instance['title'], ENT_QUOTES);
+        ?>
+<div>
+    <p>
+        <label for="<?= esc_attr($this->get_field_id('title')); ?>">
+            <?= esc_html__('Title', SMLD); ?>:
+            <input class="widefat" id="<?= esc_attr($this->get_field_id('title')); ?>" name="<?= esc_attr($this->get_field_name('title')); ?>" type="text" value="<?= esc_attr($title); ?>" />
+        </label>
+    </p>
+</div>
+        <?php
+    }
 }
